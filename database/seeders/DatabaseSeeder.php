831d9350c68093bff3b6d614aa209445d2f3d62f
MRootxx <dr.rootvb@gmail.com>
2026-03-10 20:02:41 +0300

    Fix: DatabaseSeeder column name and call RolesAndPermissionsSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            RolesAndPermissionsSeeder::class,
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
+            'full_name' => 'Test User',
             'email' => 'test@example.com',
         ]);
     }
