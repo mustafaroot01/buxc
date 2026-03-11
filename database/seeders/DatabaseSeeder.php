850c0c3ea02b43a77e2de1be9811c467d68843d6
MRootxx <dr.rootvb@gmail.com>
2026-03-11 17:57:36 +0300

    Remove test user factory from DatabaseSeeder to fix seeding error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,14 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
@@ -17,11 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolesAndPermissionsSeeder::class,
-        ]);
-
-        User::factory()->create([
-            'full_name' => 'Test User',
-            'email' => 'test@example.com',
         ]);
     }
 }
